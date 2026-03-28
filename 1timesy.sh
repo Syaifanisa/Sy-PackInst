63c3bae7df2f954cc7e37e4cb19a17bc2f7419cb
Syaifani S.A <syaifani.s.anugerah@gmail.com>
2026-03-28 21:50:58 +0800

    Fix formatting and improve script commands

--- 1timesy.sh
+++ 1timesy.sh
@@ -4,11 +4,10 @@ command -v curl
 pkg install -y curl || apt install -y curl || dnf install -y curl --skip-broken || yum install -y curl --skip-broken || pacman -S --noconfirm curl
 
 mkdir -p ~/Sy-PackInst
-curl -o ~/Sy-PackInst/Sy-PackInst.py \
-https://raw.githubusercontent.com/Syaifanisa/Sy-PackInst/main/Sy-PackInst.py
+curl -o ~/Sy-PackInst/Sy-PackInst.py \ https://raw.githubusercontent.com/Syaifanisa/Sy-PackInst/main/Sy-PackInst.py
 echo "[+] Making Alias"
-echo "alias Sy-PackInst=' cd ~/Sy-PackInst && python3 Sy-PackInst.py && cd -'" >> ~/.bashrc
+echo "alias Sy-PackInst='cd ~/Sy-PackInst && python3 Sy-PackInst.py && cd -'" >> ~/.bashrc
 echo "[+] Running back shell"
 source ~/.bashrc
-echo "[+] Done, type 'Sy-PackInst' to run it." 
+echo "[+] Done, type 'Sy-PackInst' to run it."
 rm -- "$0"
