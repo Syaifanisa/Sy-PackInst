2d8bbfade44b88c55c509a3ba2e1d70bd4324f20
Syaifani S.A <syaifani.s.anugerah@gmail.com>
2026-03-28 21:30:13 +0800

    Update 1timesy.sh

--- 1timesy.sh
+++ 1timesy.sh
@@ -1,15 +1,7 @@
 echo "[+] Starting..." 
-pkg install -y python3
-apt install -y python3
-dnf install -y python3 --skip-broken
-yum install -y python3 --skip-broken
-pacman -S --noconfirm python3
+pkg install -y python3 || apt install -y python3 ||dnf install -y python3 --skip-broken || yum install -y python3 --skip-broken || pacman -S --noconfirm python3
 command -v curl
-pkg install -y curl
-apt install -y curl
-dnf install -y curl --skip-broken
-yum install -y curl --skip-broken
-pacman -S --noconfirm curl
+pkg install -y curl || apt install -y curl || dnf install -y curl --skip-broken || yum install -y curl --skip-broken || pacman -S --noconfirm curl
 
 mkdir -p ~/Sy-PackInst
 curl -o ~/Sy-PackInst/Sy-PackInst.py \
